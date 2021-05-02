fd30b27f0ee3058a72d6bdc4d2c2f1cb25eee7e7
Sotniko89git config --global user.name Sotniko89git config --global user.name Sotniko89 <sotnikovvu@gmail.com>
2021-05-03 01:42:18 +0300

    add hide classes and properties

---
+++ src/main/java/ru/accident/AccidentApplication.java
@@ -0,0 +1,15 @@
+package ru.accident;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+@SpringBootApplication
+public class AccidentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccidentApplication.class, args);
+    }
+
+}
+*/
---
+++ src/test/java/ru/accident/AccidentApplicationTests.java
@@ -0,0 +1,15 @@
+package ru.accident;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/*
+@SpringBootTest
+class AccidentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
+*/
