1099abba604c3a5e88b95d2d3360cf0590844f05
Sotniko89git config --global user.name Sotniko89git config --global user.name Sotniko89 <sotnikovvu@gmail.com>
2021-05-11 23:55:02 +0300

    fix tests for building

--- src/test/java/ru/accident/controllers/LoginControllerTest.java
+++ src/test/java/ru/accident/controllers/LoginControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import ru.accident.AccidentApplication;
@@ -19,6 +20,7 @@ class LoginControllerTest {
     MockMvc mockMvc;
 
     @Test
+    @WithMockUser
     void loginPage() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/login"))
                 .andExpect(status().isOk())
--- src/test/java/ru/accident/controllers/RegControlTest.java
+++ src/test/java/ru/accident/controllers/RegControlTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import ru.accident.AccidentApplication;
@@ -30,6 +31,7 @@ class RegControlTest {
     MockMvc mockMvc;
 
     @Test
+    @WithMockUser
     void reg() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/reg"))
                 .andExpect(status().isOk())
